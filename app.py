c9a42ad2b2f4fbe1ad048d34ca19d31f6a9eac20
mistest <mehrotra.prakhar1@gmail.com>
2024-06-25 18:02:55 +0530

    Fixing If else issue syntax

--- app.py
+++ app.py
@@ -6,16 +6,14 @@ logging.basicConfig(level = logging.INFO)
 def connect_to_mw_dict(key, dict):
   logging.info("Connecting to Merriam-Webster Dictionary")
   url = "https://dictionaryapi.com/api/v3/references/sd3/json/"+dict+"?key="+key
-  print (url)
+  attr = {'word': dict,'key': key}
   req = requests.get(url = url, params = attr)
-  
+
   if req.status_code == 200:
     logging.info("Connection successful to MW ")
-
-def main():
-  key=os.environ["API_KEY"]
-  dict= sys.argv[1] 
-  outcome, result_json = connect_to_mw_dict(key, dict)
+    return True, req.json()
+  else:
+    print("Connection failed to MW.")
 
 
 if __name__ == "__main__":
