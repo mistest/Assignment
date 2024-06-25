116545eba4b5d00332b46e2aed555985e42ac42d
mistest <mehrotra.prakhar1@gmail.com>
2024-06-25 18:01:43 +0530

    Checking If conditions for Status 200

--- app.py
+++ app.py
@@ -3,12 +3,14 @@ import logging
 logging.basicConfig(level = logging.INFO)
 
 
-
 def connect_to_mw_dict(key, dict):
   logging.info("Connecting to Merriam-Webster Dictionary")
   url = "https://dictionaryapi.com/api/v3/references/sd3/json/"+dict+"?key="+key
   print (url)
-
+  req = requests.get(url = url, params = attr)
+  
+  if req.status_code == 200:
+    logging.info("Connection successful to MW ")
 
 def main():
   key=os.environ["API_KEY"]
